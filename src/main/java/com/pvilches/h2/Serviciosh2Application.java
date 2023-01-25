be51221e77abb14cb8f3338df064ac818d08b724
npablo1987 <kyovilches@gmail.com>
2023-01-25 10:24:01 -0400

    Initial commit

---
+++ src/main/java/com/pvilches/h2/Serviciosh2Application.java
@@ -0,0 +1,13 @@
+package com.pvilches.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serviciosh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Serviciosh2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pvilches/h2/Serviciosh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvilches.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serviciosh2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
